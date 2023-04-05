f2e8453699d0149873ef9c70ecbaf488a4e49bef
Viacheslav <108462275+ViacheslavYUR@users.noreply.github.com>
2023-04-06 00:53:45 +0300

    done

---
+++ NewProject/App.js
@@ -0,0 +1,11 @@
+import RegistrationScreen from "./Screens/Auth/RegistrationScreen";
+import LoginScreen from "./Screens/Auth/LoginScreen";
+
+export default function App() {
+  return (
+    <>
+      {/* <LoginScreen /> */}
+      <RegistrationScreen />
+    </>
+  );
+}
